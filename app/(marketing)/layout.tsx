ce0c6aefef1af2c60aa5f674b0f8c7a7b9ded8c5
sadegh akbari <akbarisadegh382@gmail.com>
2023-11-30 23:58:01 +0330

    feat: added clerk config

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -5,7 +5,7 @@ const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full bg-slate-200">
       <Navbar />
-      <main className="pt-40 pb-20 bg-slate-200">{children}</main>
+      <main className="pt-24 pb-20 bg-slate-200">{children}</main>
       <Footer />
     </div>
   )
