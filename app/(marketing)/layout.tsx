1f97b44c33542a516966939723ccfd8d0ccb38d3
sadegh akbari <akbarisadegh382@gmail.com>
2023-11-30 12:57:46 +0330

    feat: added landing page

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,4 +1,10 @@
 const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>
+  return (
+    <div className="h-full bg-slate-200">
+      {/* navbar */}
+      <main className="pt-40 pb-20 bg-slate-200">{children}</main>
+      {/* footer */}
+    </div>
+  )
 }
 export default MarketingLayout
